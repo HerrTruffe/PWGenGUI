7aac74d58348576c4d8474b888919130f2465f01
Robin Schroeder <robin.schroeder@egotec.com>
2024-11-27 16:23:22 +0100

    Anpassung für die Speicherung eines Passwortes in eine Json-Datei

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.egotec.pwgengui {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
+    requires org.json;
 
 
     opens com.egotec.pwgengui to javafx.fxml;
